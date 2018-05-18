72d287888633280adb4ecefa539e36041f91c129
corytveten <corytveten@gmail.com>
2018-05-18 16:28:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,6 +47,12 @@ class Song
     sorted
   end
 
-  def self.new_from_filename
+  def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.new = song_array[1]
+    song.artist_name = song_array[0]
+    song
   end
 end
