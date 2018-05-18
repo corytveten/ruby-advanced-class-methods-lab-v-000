9a6d8a54f0276e4d36d5ebd9fc9d2941abe18220
corytveten <corytveten@gmail.com>
2018-05-18 16:26:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,13 +22,13 @@ class Song
     song
   end
 
-  def create_by_name(title)
+  def self.create_by_name(title)
     song = self.create
     song.name = title
     song
   end
 
-  def find_by_name(title)
+  def self.find_by_name(title)
     result = self.all.detect { |song| song.name == title }
     result
   end
@@ -46,5 +46,7 @@ class Song
     sorted = self.all.sort_by { |song| song.name }
     sorted
   end
+
+  def self.new_from_filename
 
 end
